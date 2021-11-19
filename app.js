9533764ebc85104664822022c807c71921f0d1ff
Fernando <flopezlopez6@outlook.com>
2021-11-18 20:37:45 -0600

    Sign in wip

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./database");
-
+const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -9,6 +9,7 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+// DB ROUTES AND QUERIES
 app.post("/login", function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -30,6 +31,7 @@ app.post("/login", function (req, res, next) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Servidor funcionando wii");
+// PORT
+app.listen(PORT, function () {
+  console.log(`Server is listening to port ${PORT}`);
 });
