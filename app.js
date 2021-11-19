5ad61da44a9c09202a716b23f610f07695e1d239
Fernando <flopezlopez6@outlook.com>
2021-11-18 20:28:02 -0600

    Login added

--- app.js
+++ app.js
@@ -12,10 +12,21 @@ app.get("/", function (request, response) {
 app.post("/login", function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
-  var sql = `SELECT *, NULL AS contrasena FROM talento WHERE correo = '${email}' AND contrasena = '${password}';`;
-  db.query(sql, function (err, data) {
+  var sqlTalento = `SELECT *, NULL AS contrasena FROM talento WHERE correo = '${email}' AND contrasena = '${password}';`;
+  var sqlCazador = `SELECT *, NULL AS contrasena FROM cazador WHERE correo = '${email}' AND contrasena = '${password}';`;
+  db.query(sqlTalento, function (err, data) {
     if (err) throw err;
-    res.send(JSON.stringify(data));
+    valueTalento = JSON.stringify(data);
+    // If talento does not exists, look for cazador
+    if (valueTalento === "[]") {
+      db.query(sqlCazador, function (errCazador, dataCazador) {
+        if (errCazador) throw err;
+        valueCazador = JSON.stringify(dataCazador);
+        res.send(valueCazador);
+      });
+    } else {
+      res.send(valueTalento);
+    }
   });
 });
 
